b031f4f1751842bb4d9b0e44d7271c38213ce48d
aditishaw308 <92675430+aditishaw308@users.noreply.github.com>
2021-10-17 18:23:34 +0530

    Update Bubble_sort.cpp

--- DSA Practice/Bubble_sort.cpp
+++ DSA Practice/Bubble_sort.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter your array length:-\n";
+    cout<<"Enter the array length:-\n";
     cin>>n;
     int arr[n];
-    cout<<"Enter the array elements:-\n";
+    cout<<"Enter your array elements:-\n";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     cout<<"Your unsorted array:-\n";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<ends;
-    }
+    for(int j=0;j<n;j++)
+        cout<<arr[j]<<ends;
     cout<<endl;
     // Sorting process
     int x=1;
