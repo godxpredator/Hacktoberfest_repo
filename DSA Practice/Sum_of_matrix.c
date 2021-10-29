50134f395cbafd2e2f47fc44818485669fe6869b
arnab123dotcom <69048126+arnab123dotcom@users.noreply.github.com>
2021-10-29 21:49:34 +0530

    Update Sum_of_matrix.c

--- DSA Practice/Sum_of_matrix.c
+++ DSA Practice/Sum_of_matrix.c
@@ -3,14 +3,15 @@ void main(){
     int rows , cols;
     printf("Enter Rows and Colomns for both Matrices:-\n");
     scanf("%d%d",&rows,&cols);
+    //rows are horizontal columns are vertical.
     int arr1[rows][cols],arr2[rows][cols];
-    printf("Enter elements of your first array:-\n");
+    printf("Enter the elements of your first array:-\n");
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
             scanf("%d",&arr1[i][j]);
         }
     }
-    printf("Enter elements of your second array:-\n");
+    printf("Enter the elements of your second array:-\n");
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
             scanf("%d",&arr2[i][j]);
@@ -26,7 +27,7 @@ void main(){
     printf("Sum of the matrices is:-\n");
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
-            printf(" %d ",sum[i][j]);
+            printf("%d ",sum[i][j]);
         }
         printf("\n");
     }
